a9801dfaa56ed1047f0dd53cf5ec1b229f18b5d0
Guilherme <mttsguil@gmail.com>
2022-10-18 15:20:11 -0300

    add: strings in ruby

--- modulo_02/07_hash.rb
+++ modulo_02/07_hash.rb
@@ -3,3 +3,4 @@ h = {"x"=> 10, "y" => 20, "curso" => "rails"}
 puts h["x"].to_i + h["y"].to_i
 
 puts h["curso"]
+
---
+++ modulo_02/08_strings.rb
@@ -0,0 +1,10 @@
+x = "Jackson"
+y = "Rails"
+
+puts x
+puts y
+
+puts x + y 
+puts x << y #Modifica o A
+
+puts "O #{x} gosta do curso de #{y}"
