e01c5dfe81ac8775e27dbede54cd906ea9485655
Guilherme <mttsguil@gmail.com>
2022-10-18 15:52:43 -0300

    add: params and initialize

--- modulo_02/10_pessoa.rb
+++ modulo_02/10_pessoa.rb
@@ -1,9 +1,21 @@
 class Pessoa
-    def falar
-        "Olá, pessoal!"
+
+    def initialize(cont = 5)
+        cont.times do |i|
+        "Inicializando... #{i}"
+        end
+    end
+
+    def falar(falar  = "Pessoal")
+        "Olá, #{falar}!"
+    end
+
+    def falar2(falar = "Pessoal", texto2 = "tudo bem?")
+        "Olá, #{falar} #{texto2}!"
     end
 end
 
 p = Pessoa.new
 
-puts p.falar 
+puts p
+puts p.falar2("Guilherme", "como vai!") 
