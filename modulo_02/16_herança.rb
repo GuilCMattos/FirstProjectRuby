fedcb2df64977b63d73f12092d031be7ff234745
Guilherme <mttsguil@gmail.com>
2022-10-18 16:15:58 -0300

    add: herança

---
+++ modulo_02/16_herança.rb
@@ -0,0 +1,27 @@
+class Pessoa
+    attr_accessor :nome, :email
+end
+
+class PessoaFisica < Pessoa 
+    attr_accessor :cpf
+
+    def falar(texto)
+        texto
+    end
+end
+
+class PessoaJuridica < Pessoa
+    attr_accessor :cnpj
+    
+    def pagar_fornecedor
+        "Pago..."
+    end
+end
+
+p1 = PessoaFisica.new
+
+p1.nome = "Guilherme"
+p1.email = "mttsguil@gmail.com"
+p1.cpf = "394.555.234.09"
+puts "#{p1.nome} #{p1.email} #{p1.cpf}" 
+puts p1.falar("Salve")
